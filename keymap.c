b034f9897755ac3e2d518d04cacda46e80790aaa
Hannah Morrison <hannah@landhmorrison.scot>
2023-08-10 14:28:25 +0100

    Add homerow mods to number keys

--- keymap.c
+++ keymap.c
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NAV_LAYER]          = LAYOUT_planck_grid(
-    KC_1,              KC_2,          KC_3,                 KC_4,                 KC_5,     KC_TRNS,         KC_TRNS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-    KC_TAB,            LCTL(KC_TAB),  M_ALTT,               KC_BTN1,              KC_BTN2,  KC_TRNS,         KC_TRNS,  KC_WH_U,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,
-    TO(FUNC_LAYER),    LCTL(LGUI(KC_LEFT)),  LCTL(LGUI(KC_RGHT)), M_SCM1,  M_ESCV,   KC_TRNS,         KC_TRNS,  KC_WH_D,  KC_HOME,  KC_PGDN,  KC_PGUP,  OSL(SCUT_LAYER),
-    KC_TRNS,           KC_TRNS,       KC_TRNS,              KC_TRNS,              KC_TRNS,  TO(BASE_LAYER),  KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
-                       ),
+    LSFT_T(KC_1),    LCTL_T(KC_2),         LALT_T(KC_3),         LGUI_T(KC_4),  KC_5,     KC_TRNS,         KC_TRNS,  KC_6,     LGUI_T(KC_7),  LALT_T(KC_8),  LCTL_T(KC_9),  LSFT_T(KC_0),
+    KC_TAB,          LCTL(KC_TAB),         M_ALTT,               KC_BTN1,       KC_BTN2,  KC_TRNS,         KC_TRNS,  KC_WH_U,  KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,
+    TO(FUNC_LAYER),  LCTL(LGUI(KC_LEFT)),  LCTL(LGUI(KC_RGHT)),  M_SCM1,        M_ESCV,   KC_TRNS,         KC_TRNS,  KC_WH_D,  KC_HOME,       KC_PGDN,       KC_PGUP,       OSL(SCUT_LAYER),
+    KC_TRNS,         KC_TRNS,              KC_TRNS,              KC_TRNS,       KC_TRNS,  TO(BASE_LAYER),  KC_NO,    KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
+  ),
 
   [FUNC_LAYER] = LAYOUT_planck_grid(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_MNXT,  KC_TRNS,         KC_TRNS,        KC_VOLU,  KC_BRIU,  KC_ASTR,  KC_NO,    KC_PLUS,
